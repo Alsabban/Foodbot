8c71792c8ca9e9a4ab0f9d29f476b6bca8ce771b
MostafaShamsss <mosstafashamss@gmail.com>
2021-09-22 17:08:51 +0200

    final rivescript work

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ con = mysql.connect(host="localhost" , user="root" , password="" , db="chatbot")
 
 def saveRest(name,rest_id):
     cur = con.cursor(dictionary=True)
-    qry = "INSERT INTO 'restaurants' (name , id) VALUES(%s,%s)"
+    qry = "INSERT INTO restaurants (name , id) VALUES(%s,%s)"
     val = (name, rest_id)
     cur.execute(qry, val)
     con.commit()
